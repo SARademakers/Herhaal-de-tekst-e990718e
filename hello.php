b9c189ac62354a021124d532992c8d2062e7c99e
SARademakers <2049137@talnet.nl>
2019-09-30 14:56:53 +0200

    Uitwerking van de exercise

---
+++ hello.php
@@ -0,0 +1,9 @@
+<?php
+echo ("Hello world" . PHP_EOL);
+echo ("Hello world" . PHP_EOL);
+echo ("Hello world" . PHP_EOL);
+echo ("Hello world" . PHP_EOL);
+echo ("Hello world" . PHP_EOL);
+echo ("Hello world" . PHP_EOL);
+echo ("Hello world" . PHP_EOL);
+echo ("Hello world" . PHP_EOL);
